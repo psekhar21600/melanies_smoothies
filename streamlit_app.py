dffc33fbcc534e4368089367180edfbd2df05ef5
psekhar21600 <p.sekhar@mphasis.com>
2025-02-25 14:15:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ st.write("The name on your Smoothie will be:", name_on_your_order)
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 # Convert the snowark Dataframe to a pandas DataFrame so we can use the LOC Function
 pd_df = my_dataframe.to_pandas()
