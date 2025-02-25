a46f78ec35aebfee762698038b65374247546995
psekhar21600 <p.sekhar@mphasis.com>
2025-02-25 14:31:37 +0530

    Update streamlit_app.py
    #st.dataframe(pd_df)

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the snowark Dataframe to a pandas DataFrame so we can use the LOC Function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 #st.stop
 
 ingredients_list = st.multiselect(
