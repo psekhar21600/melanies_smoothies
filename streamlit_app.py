83561003fc6181b529a9c8006d59d47713cc09ae
psekhar21600 <p.sekhar@mphasis.com>
2025-02-25 13:51:14 +0530

    Update streamlit_app.py
    In For loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+        
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
