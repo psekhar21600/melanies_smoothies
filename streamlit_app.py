8d70b8957ea97fde9f9c2556f5ecf9b3a3fed2bb
psekhar21600 <p.sekhar@mphasis.com>
2025-02-25 13:39:12 +0530

    Update streamlit_app.py
    my_dataframe (un comment)

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
