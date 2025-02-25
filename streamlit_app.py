90df1d07498593ea3163b9fac7416a83e818c641
psekhar21600 <p.sekhar@mphasis.com>
2025-02-25 14:23:09 +0530

    Update streamlit_app.py
    sf

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the snowark Dataframe to a pandas DataFrame so we can use the LOC Function
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -45,7 +45,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ SEARCH_ON)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     #st.write (ingredients_string)
